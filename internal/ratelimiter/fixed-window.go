cca925d1794c6db9c492955abb66a4b7eb583291
ecetinerdem <cetoerdem1@gmail.com>
2025-12-01 21:47:36 +0400

    ratelimiter in memory done

--- internal/ratelimiter/fixed-window.go
+++ internal/ratelimiter/fixed-window.go
@@ -20,3 +20,30 @@ func NewFixedWindowLimiter(limit int, window time.Duration) *FixedWindowRateLimi
 		window:  window,
 	}
 }
+
+func (fwrl *FixedWindowRateLimiter) Allow(ip string) (bool, time.Duration) {
+	fwrl.Lock()
+	count, exist := fwrl.clients[ip]
+	fwrl.Unlock()
+
+	if !exist || count < fwrl.limit {
+		fwrl.Lock()
+
+		if !exist {
+			go fwrl.resetCount(ip)
+		}
+		fwrl.clients[ip]++
+		fwrl.Unlock()
+		return true, 0
+	}
+
+	return false, fwrl.window
+}
+
+func (fwrl *FixedWindowRateLimiter) resetCount(ip string) {
+	time.Sleep(fwrl.window)
+
+	fwrl.Lock()
+	delete(fwrl.clients, ip)
+	fwrl.Unlock()
+}
