ed4d016a6d07abb1460383144a599c3e1ad11b70
ecetinerdem <cetoerdem1@gmail.com>
2025-12-02 16:44:07 +0400

    feat:add confirmation page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,25 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { ConfirmationPage } from './ConfirmationPage.tsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/confirm/:token",
+    element: <ConfirmationPage />
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
